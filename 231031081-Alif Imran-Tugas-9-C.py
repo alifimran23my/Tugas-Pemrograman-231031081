084b347bb6d7775e1a107f99eeb31a1021f69bb6
alifimran23my <153783001+alifimran23my@users.noreply.github.com>
2023-12-21 18:18:45 +0800

    Add file

---
+++ 231031081-Alif Imran-Tugas-9-C.py
@@ -0,0 +1,17 @@
+vd = {"nama" : "Alif Imran",
+      "kelas" : "SI23-C",
+      "nim" : "231031081",
+      "asal" : "Baringin",
+      "tanggal" : "01-Januari-2004",
+      "alamat" : "Jalan kelapa gading",
+      "hobi" : "Bulutangkis",
+      "tinggi" : "164 cm",
+      }
+print("Nama Lengkap  :",vd["nama"])
+print("Kelas         :",vd["kelas"])
+print("NIM           :",vd["nim"])
+print("Tmpt tgl lahir:",vd["asal"],vd["tanggal"])
+print("Alamat        :",vd["alamat"])
+print("Hobi          :",vd["hobi"])
+print("Tinggi        :",vd["tinggi"])
+print("\nTipe data var vd : ",type(vd))
